cd0f7cd467d94786b8ee5ac115890bc007723b77
Mark Rawls <markrawls96@gmail.com>
2020-10-27 17:22:01 -0400

    Change route to bug fables route

--- src/handlers/information/index.js
+++ src/handlers/information/index.js
@@ -6,7 +6,18 @@ import gloves from './gloves';
 import jackbox from './jackbox';
 import merch from './merch';
 import commands from './commands';
+import route from './route';
 
-const informationHandlers = [art, avDiscord, discord, friendCode, gloves, jackbox, merch, commands];
+const informationHandlers = [
+  art,
+  avDiscord,
+  discord,
+  friendCode,
+  gloves,
+  jackbox,
+  merch,
+  commands,
+  route,
+];
 
 export default informationHandlers;
---
+++ src/handlers/information/route.js
@@ -0,0 +1,7 @@
+const route = async ({ say }) => {
+  say('https://docs.google.com/document/d/1IfBkJWZN38BnDfQHNrMS5Ij9GZhN8zKfXhXN8mud4O8/edit');
+};
+
+route.command = 'route';
+
+export default route;
