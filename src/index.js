b749cf98f54bc60211065da8e811177a24a2b13f
Mark Rawls <markrawls96@gmail.com>
2020-11-03 17:53:47 -0500

    Move parsing library to its own module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { ChatClient } from 'dank-twitch-irc';
-import { split } from 'shlex';
+import parse from 'whisparse';
 
 import handlers from 'handlers';
 import { getPermissionsLevel, wrapHandlerFunc } from 'utils';
@@ -26,25 +26,19 @@ client.on('close', error => {
 
 client.on('message', async input => {
   const message = { ...input };
-  if (!message.messageText || !message.messageText.startsWith('!')) {
-    return;
-  }
+  const parsed = parse(message.messageText);
 
-  const parsed = split(message.messageText);
-
-  if (!parsed.length) {
+  if (!parsed) {
     return;
   }
 
   const vips = await client.getVips(channelName);
-  const [command, ...args] = parsed;
 
   message.isStreamer = message.channelName === message.senderUsername;
   message.isVip = vips.includes(message.senderUsername);
 
   message.permissionsLevel = getPermissionsLevel(message);
 
-  const argsString = message.messageText.replace(`${command} `, '');
   const commands = await getCollection('commands');
   let dbCommands = await commands.find({}).toArray();
   dbCommands = dbCommands.map(d => {
@@ -55,9 +49,7 @@ client.on('message', async input => {
 
   for (const handler of handlers.concat(dbCommands)) {
     handler({
-      command: command.slice(1).toLowerCase(),
-      args,
-      argsString,
+      ...parsed,
       message,
       say: client.say.bind(client, channelName),
     });
